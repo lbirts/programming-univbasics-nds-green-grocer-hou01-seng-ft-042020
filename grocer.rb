b475433c991d113e8616ce2b13867588784c9518
Lauren Birts <birtslauren@gmail.com>
2020-03-24 19:35:43 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -2,6 +2,15 @@ def find_item_by_name_in_collection(name, collection)
   # Implement me first!
   #
   # Consult README for inputs and outputs
+  item_index = 0
+  while collection[item_index] do
+    item = collection[item_index]
+  if name == item[:item]
+    return item
+  end
+  item_index += 1
+end
+nil
 end
 
 def consolidate_cart(cart)
@@ -9,18 +18,60 @@ def consolidate_cart(cart)
   #
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
+  item_index = 0
+  unique_array = []
+  while cart[item_index] do
+    item = cart[item_index]
+    item_hash = find_item_by_name_in_collection(item[:item], unique_array)
+    if item_hash
+      item_hash[:count] += 1
+    else 
+      item_hash = item
+      item_hash[:count] = 1
+      unique_array << item_hash
+    end
+    item_index += 1
+  end
+  unique_array
 end
+
 
 def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
+  item_index = 0
+  while cart[item_index] do
+    item = cart[item_index]
+    coupon_item = find_item_by_name_in_collection(item[:item], coupons)
+    if coupon_item
+      item[:count] -= coupon_item[:num]
+      cart << {
+        :item => "#{coupon_item[:item]} W/COUPON",
+        :price => (coupon_item[:cost] / coupon_item[:num]),
+        :clearance => item[:clearance],
+        :count => coupon_item[:num]
+      }
+    end
+    item_index += 1
+  end
+  cart
 end
 
 def apply_clearance(cart)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
+  item_index = 0
+  while cart[item_index] do
+    item = cart[item_index]
+    if item[:clearance] == true
+      item[:price] -= (item[:price] * 0.2)
+    end
+    cart << item[:price]
+    item_index += 1
+  end
+  cart
 end
 
 def checkout(cart, coupons)
